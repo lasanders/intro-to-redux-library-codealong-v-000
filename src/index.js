8142d179fee69bf271c4f9e5be5678b6c2dab4d2
Lara Sanders <larasanders@gmail.com>
2019-06-16 21:05:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,4 +13,10 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        {' '}
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
